173460e6c964a3b8d58121b39153dfd4a2b4fd75
heltrigueiro <heltrigueiro@gmail.com>
2019-12-30 21:48:41 -0300

    Task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+      font-family: "Times New Roman", Times, serif;
+    }
+  `]
 })
 export class AppComponent {
   name = 'my-first-app';
